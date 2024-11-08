691ce844d41cc417b9bec016df9a63322761eda6
Roman Zhadko <zhar.andr.developer.wwl@gmail.com>
2024-11-08 05:50:58 +0200

    center loading component

--- app/src/main/java/com/zhadko/productsmanager/ui/screens/productListScreen/ProductsListScreen.kt
+++ app/src/main/java/com/zhadko/productsmanager/ui/screens/productListScreen/ProductsListScreen.kt
@@ -1,9 +1,9 @@
 package com.zhadko.productsmanager.ui.screens.productListScreen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -11,7 +11,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.zhadko.productsmanager.ui.components.AddButton
 import com.zhadko.productsmanager.ui.components.ProductsList
@@ -25,11 +24,16 @@ fun ProductsListScreen(
     val uiState by viewModel.state.collectAsState()
 
     if (uiState.isLoading) {
-        CircularProgressIndicator(
-            modifier = Modifier.width(64.dp),
-            color = MaterialTheme.colorScheme.secondary,
-            trackColor = MaterialTheme.colorScheme.surfaceVariant,
-        )
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier,
+                color = MaterialTheme.colorScheme.secondary,
+                trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            )
+        }
     } else {
         Column(
             modifier = Modifier.fillMaxSize(),
