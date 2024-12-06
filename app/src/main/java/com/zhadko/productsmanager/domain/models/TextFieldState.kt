8aa62603c708983ec3f05f65a5fcdde689e59e15
Roman Zhadko <zhadkoroman@gmail.com>
2024-12-06 02:36:58 +0200

    clear unused code

--- app/src/main/java/com/zhadko/productsmanager/domain/models/TextFieldState.kt
+++
@@ -1,9 +0,0 @@
-package com.zhadko.productsmanager.domain.models
-
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-
-class TextFieldState {
-    var text: String by mutableStateOf("")
-}
